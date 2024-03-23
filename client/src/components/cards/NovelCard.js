e0a2b945e4f4084d253ed2d3c0cd6efad80c0c01
Muhammad-Farhan <73163937+farhan26azam@users.noreply.github.com>
2024-03-23 18:04:20 +0500

    Update NovelCard.js

--- client/src/components/cards/NovelCard.js
+++ client/src/components/cards/NovelCard.js
@@ -1,6 +1,6 @@
 const NovelCard = ({ novel }) => {
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+    <div className="w-96 p-6 bg-white border border-gray-200 rounded-lg shadow">
       <a href={`/story/${novel._id}`}>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
           {novel?.title || "Title"}
